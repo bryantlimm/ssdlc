0aeea9d23ed17ca636ff92866ba376a8027d4d10
bryantlimm <bryant.aryadi@student.sgu.ac.id>
2025-11-13 11:26:31 +0700

    second commit

--- test.js
+++ test.js
@@ -34,11 +34,11 @@ describe('validatePassword', () => {
 // --- Intentional Break Test (Fails on Purpose) ---
 
 // **IMPORTANT**: You must comment out or remove this test for your "Passing Run" submission!
-describe('Intentional Fail Test', () => {
-    // 5. Intentional Break Test (Fails on Purpose)
-    test('should fail because the expected outcome is intentionally wrong', () => {
-        // We know 'StrongP@ss123' is a valid password (returns true), but we expect it to be false.
-        // This forces a test failure, thus failing the CI workflow.
-        expect(validatePassword('StrongP@ss123')).toBe(false); // <--- This line causes the intentional failure!
-    });
-});
+// describe('Intentional Fail Test', () => {
+//     // 5. Intentional Break Test (Fails on Purpose)
+//     test('should fail because the expected outcome is intentionally wrong', () => {
+//         // We know 'StrongP@ss123' is a valid password (returns true), but we expect it to be false.
+//         // This forces a test failure, thus failing the CI workflow.
+//         expect(validatePassword('StrongP@ss123')).toBe(false); // <--- This line causes the intentional failure!
+//     });
+// });
